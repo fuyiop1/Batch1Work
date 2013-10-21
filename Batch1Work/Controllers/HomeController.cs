4cf825c4b4a9c878bf40dddd81427bc9a8cf71da
fuyiop1 <1733209296@qq.com>
2013-10-21 11:24:32 +0800

    add resize function and apply filters for all columns

--- Batch1Work/Controllers/HomeController.cs
+++ Batch1Work/Controllers/HomeController.cs
@@ -25,6 +25,66 @@ namespace Batch1Work.Controllers
             return Json(ActionGridItems.Select(x => x.Building).Distinct(), JsonRequestBehavior.AllowGet);
         }
 
+        public ActionResult FilterMenuCustomization_Floor()
+        {
+            return Json(ActionGridItems.Select(x => x.Floor).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_Area()
+        {
+            return Json(ActionGridItems.Select(x => x.Area).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_Item()
+        {
+            return Json(ActionGridItems.Select(x => x.Item).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_ProductType()
+        {
+            return Json(ActionGridItems.Select(x => x.ProductType).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_AsbestosType()
+        {
+            return Json(ActionGridItems.Select(x => x.AsbestosType).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_Quantity()
+        {
+            return Json(ActionGridItems.Select(x => x.Quantity).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_InspFreq()
+        {
+            return Json(ActionGridItems.Select(x => x.InspFreq).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_MaScore()
+        {
+            return Json(ActionGridItems.Select(x => x.MaScore).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_PaScore()
+        {
+            return Json(ActionGridItems.Select(x => x.PaScore).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_RiskScore()
+        {
+            return Json(ActionGridItems.Select(x => x.RiskScore).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_RiskCategory()
+        {
+            return Json(ActionGridItems.Select(x => x.RiskCategory).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_RecAction()
+        {
+            return Json(ActionGridItems.Select(x => x.RecAction).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
         public virtual JsonResult GetActionGridItems([DataSourceRequest] DataSourceRequest request)
         {
             DataSourceResult result = ActionGridItems.ToDataSourceResult(request);
